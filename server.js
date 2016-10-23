28721b6160e6201b56b34b8065dc98147e70a443
Gerd Busker <gerd@busker.org>
2016-10-24 00:45:05 +0100

    Added bootstrap, pug

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+// Set up the pug view engine
+app.set('views', './app/views');
+app.set('view engine', 'pug');
+
+// Start server on port 3000
 app.listen(3000, function() {
   console.log("Server started.");
 });
 
+// Serve static assets from "public"
 app.use(express.static('public'));
+
+// Serve templates
+app.get('/', function(req, res) {
+  res.render('index', {});
+});
