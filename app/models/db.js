4fcd24ca1d16fae6d481c17e5ca8ff7ecf7c2db3
Gerd Busker <gerd@busker.org>
2016-11-12 14:40:53 +0000

    heroku support for mLab

--- app/models/db.js
+++ app/models/db.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose')
 // var URI = 'mongodb://devuser:FireSkinMan@ds057806.mlab.com:57806/mathsquiz'
-var URI = 'mongodb://localhost/mathsquiz';
+var URI
+if ( process.env.MONGODB_URI) {
+  URI = process.env.MONGODB_URI
+} else {
+  URI = 'mongodb://localhost/mathsquiz'
+}
 mongoose.connect(URI);
 
 // CONNECTION EVENTS
