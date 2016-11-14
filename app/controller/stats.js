fd8b573141818be576a156d6edb27f4d9febf12f
Gerd Busker <gerd@busker.org>
2016-11-14 18:04:04 +0000

    stats only show finished

--- app/controller/stats.js
+++ app/controller/stats.js
@@ -5,7 +5,7 @@ var Team =   mongoose.model('Team')
 module.exports = {
 
   index: function(req, res) {
-    Team.stats(function(err, teams) {
+    Team.statsFinished(function(err, teams) {
       res.render('stats/index', {teams: teams})
     })
   }
